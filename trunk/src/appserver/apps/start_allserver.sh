ec4be20f5e799aa5d9ffb96e9301c023dc5c1e57
yawei <xdjm562@qq.com>
2017-05-26 10:36:52 +0800

    add launch script

---
+++ trunk/src/appserver/apps/start_allserver.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd user_srv_d/
+exec nohup sh start.sh & $*
+
+cd ../msg_srv_d/
+exec nohup sh start.sh & $*
+
+cd ../file_srv_d/
+exec nohup sh start.sh & $*
+
+cd ../terminal_srv_d/
+exec nohup sh start.sh & $*
+
+exit 1
+
---
+++ trunk/src/appserver/apps/start_allserver.sh~
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd user_srv_d/
+exec nohup sh start.sh & $*
+
+cd ../msg_srv_d/
+exec nohup sh start.sh & $*
+
+cd ../file_srv_d/
+exec nohup sh start.sh & $*
+
+cd ../terminal_srv_d/
+exec nohup sh start.sh & $*
+

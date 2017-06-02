9ffe037831562dc656ac71f0e32206c056302d30
yawei <xdjm562@qq.com>
2017-06-03 01:01:19 +0800

    svn_5.31 12:50 608

---
+++ trunk/src/appserver/lib/push_msg.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import json
+
+
+def new_device_off_line_msg():
+    msg = {"type": "device",
+           "signal": "offline", }
+    return json.dumps(msg, ensure_ascii=False, encoding="utf8")
+
+
+def new_location_change_msg(latitude, longitude, location_time, radius):
+    msg = {"type": "pet",
+           "signal": "location-change",
+           "data": {
+               "latitude": latitude,
+               "location_time": location_time,
+               "longitude": longitude,
+               "radius": radius
+           }}
+    return json.dumps(msg, ensure_ascii=False, encoding="utf8")
+
+
+def new_low_battery_msg(battery, if_ultra):
+    signal = "ultra-low-battery" if if_ultra else "low-battery"
+    msg = {"type": "device",
+           "signal": signal,
+           "data": {"battery_level": battery}}
+    return json.dumps(msg, ensure_ascii=False, encoding="utf8")

6eb70fb6eca654dcc393c95440a3d57b26bdedc8
yawei <xdjm562@qq.com>
2017-06-03 01:02:27 +0800

    svn_5.31 12:51 609

--- trunk/src/appserver/apps/file_srv_d/handlers/helper_handler.py
+++ trunk/src/appserver/apps/file_srv_d/handlers/helper_handler.py
@@ -14,66 +14,69 @@ from lib import error_codes
 from lib import xmq_web_handler
 from lib import utils
 
+
 class HelperHandler(xmq_web_handler.XMQWebHandler):
     @gen.coroutine
     @asynchronous
     def check_account_exist(self, logprefix, res, auth_type, auth_id):
         auth_dao = self.settings["auth_dao"]
-        
+
         # 检查账号是否已经存在
         phone_num = yield auth_dao.has_auth_info_by_auth_id(auth_type, auth_id)
         if phone_num is None:
-            logging.warning("%s, account not exist!, %s", logprefix, self.dump_req())
+            logging.warning("%s, account not exist!, %s", logprefix,
+                            self.dump_req())
             res["status"] = error_codes.EC_USER_NOT_EXIST
             self.res_and_fini(res)
             raise gen.Return(None)
-        
+
         raise gen.Return(phone_num)
-    
+
     @gen.coroutine
     @asynchronous
-    def check_account_exist_by_phone_num(self, logprefix, res, auth_type, phone_num):
+    def check_account_exist_by_phone_num(self, logprefix, res, auth_type,
+                                         phone_num):
         auth_dao = self.settings["auth_dao"]
-        
+
         # 检查账号是否已经存在
-        auth_id = yield auth_dao.has_auth_info_by_mobile_num(auth_type, phone_num)
+        auth_id = yield auth_dao.has_auth_info_by_mobile_num(auth_type,
+                                                             phone_num)
         if auth_id is None:
-            logging.warning("%s, account not exist!, %s", logprefix, self.dump_req())
+            logging.warning("%s, account not exist!, %s", logprefix,
+                            self.dump_req())
             res["status"] = error_codes.EC_USER_NOT_EXIST
             self.res_and_fini(res)
             raise gen.Return(None)
-        
+
         raise gen.Return(auth_id)
-    
+
     @gen.coroutine
     @asynchronous
     def check_account_status(self, logprefix, res, auth_type, auth_id):
         auth_dao = self.settings["auth_dao"]
-        
+
         # 检查账号状态
-        ec,extra = yield auth_dao.check_auth_status(auth_type, auth_id)
+        ec, extra = yield auth_dao.check_auth_status(auth_type, auth_id)
         if ec != error_codes.EC_SUCCESS:
-            logging.warning("%s, check account status failed, ec=%u extra=%s", logprefix, ec, str(extra))
+            logging.warning("%s, check account status failed, ec=%u extra=%s",
+                            logprefix, ec, str(extra))
             res["status"] = ec
             if ec == error_codes.EC_ACCOUNT_FREEZED_TEMP:
                 res["unfreeze_remain_secs"] = extra
             self.res_and_fini(res)
             raise gen.Return(False)
-    
+
         raise gen.Return(True)
-    
+
     @gen.coroutine
     @asynchronous
     def check_token(self, logprefix, res, auth_type, auth_id, token):
         auth_dao = self.settings["auth_dao"]
         ec = yield auth_dao.check_token(auth_type, auth_id, token)
         if ec != error_codes.EC_SUCCESS:
-            logging.warning("%s, check token failed, ec=%u %s", logprefix, ec, self.dump_req())
+            logging.warning("%s, check token failed, ec=%u %s", logprefix, ec,
+                            self.dump_req())
             res["status"] = ec
-            self.res_and_fini(res) 
+            self.res_and_fini(res)
             raise gen.Return(False)
         raise gen.Return(True)
-    
-    
-    
-    

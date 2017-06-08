522b424edd56e9b2ae0e591528bf0313bc28e46e
yawei <xdjm562@qq.com>
2017-06-09 00:37:20 +0800

    add pet info

--- trunk/src/appserver/apps/user_srv_d/handlers/add_pet_info.py
+++ trunk/src/appserver/apps/user_srv_d/handlers/add_pet_info.py
@@ -129,20 +129,20 @@ class AddPetInfo(HelperHandler):
             msg.battery_threshold = 25
             send_weight = weight
             send_sex = sex
-            msg.light_flash = ((0, 0),(0, 0))
+            msg.light_flash = ((0, 0),(0, 3))
             msg.pet_weight = "%.2f" % (send_weight)
             msg.pet_gender = send_sex
             logging.info("add_pet_info send_command_j03 msg:%s",msg)
-            
-            # get_res = yield terminal_rpc.send_command_params(
-            #     imei=device_imei, command_content=str(msg))
-            #
-            # if get_res["status"] == error_codes.EC_SEND_CMD_FAIL:
-            #     logging.warning("add_pet_info send_command_params, fail status:%d",
-            #                     error_codes.EC_SEND_CMD_FAIL)
-            #     res["status"] = error_codes.EC_SEND_CMD_FAIL
-                # self.res_and_fini(res)
-                # return
+
+            get_res = yield terminal_rpc.send_command_params(
+                imei=device_imei, command_content=str(msg))
+
+            if get_res["status"] == error_codes.EC_SEND_CMD_FAIL:
+                logging.warning("add_pet_info send_command_params, fail status:%d",
+                                error_codes.EC_SEND_CMD_FAIL)
+                res["status"] = error_codes.EC_SEND_CMD_FAIL
+                self.res_and_fini(res)
+                return
 
         try:
             yield pet_dao.update_pet_info_by_uid(uid, **info)

13ef353388a3852e34e2ce860f30e153bab3b5a5
yawei <xdjm562@qq.com>
2017-06-09 22:06:30 +0800

    set sport info

--- trunk/src/appserver/apps/user_srv_d/handlers/set_target_step.py
+++ trunk/src/appserver/apps/user_srv_d/handlers/set_target_step.py
@@ -29,6 +29,7 @@ class SetTargetStep(HelperHandler):
         uid = None
         pet_id = -1
         target_step = None
+        target_energy = None
 
 
         try:
@@ -37,7 +38,7 @@ class SetTargetStep(HelperHandler):
             #st = yield self.check_token("OnSetTargetStep", res, uid, token)
             #if not st:
             #    return
- 
+            target_energy = float(self.get_argument("target_energy"))
             pet_id = int(self.get_argument("pet_id", -1))
             target_step = int(self.get_argument("target_step"))
         except Exception, e:
@@ -54,7 +55,7 @@ class SetTargetStep(HelperHandler):
                 res["status"] = error_codes.EC_PET_NOT_EXIST
                 self.res_and_fini(res)
                 return          
-            yield pet_dao.update_pet_info(info["pet_id"], target_step=target_step)
+            yield pet_dao.update_pet_info(info["pet_id"], target_step=target_step, target_energy = target_energy)
         except Exception, e:
             logging.warning("OnSetTargetStep, error, %s %s", self.dump_req(), self.dump_exp(e))
             res["status"] = error_codes.EC_SYS_ERROR

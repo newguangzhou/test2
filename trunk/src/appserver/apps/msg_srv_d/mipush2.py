6b64a611782d0f8c8e42843bdf034e4f64f884a1
yawei <xdjm562@qq.com>
2017-06-14 23:21:36 +0800

    push ios

--- trunk/src/appserver/apps/msg_srv_d/mipush2.py
+++ trunk/src/appserver/apps/msg_srv_d/mipush2.py
@@ -40,10 +40,8 @@ class MiPush2:
                               extras):
         dict = json.loads(extras)
         logging.info("on send_to_alias_ios:desc:%s,dict:%s", desc, extras)
-        message = PushMessage().description("test_desc") \
-                                .sound_url("default") \
-                                .badge(1) \
-                                .category("action") \
-                                .extra(dict)
+        message = PushMessage().description("test_desc").sound_url(
+                                "default").badge(1).category(
+                                "action").extra(dict)
         recv = self._sender.send_to_alias(message.message_dict_ios(), str_uids)
         logging.debug("on send_to_alias_ios recv:%s", recv)

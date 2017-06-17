c34454bd44d664ea7e6ad495733542a8f3c60db7
yawei <xdjm562@qq.com>
2017-06-17 21:28:41 +0800

    remote login again

--- trunk/src/appserver/apps/file_srv_d/handlers/helper_handler.py
+++ trunk/src/appserver/apps/file_srv_d/handlers/helper_handler.py
@@ -79,7 +79,8 @@ class HelperHandler(xmq_web_handler.XMQWebHandler):
             res["status"] = ec
             if ec == error_codes.EC_LOGIN_IN_OTHER_PHONE:
                 res["msg"] = "您的账号已经在另一台手机登陆"
-                res["x_os_name"] = info["device_model"]
+                res["device_model"] = info["device_model"]
+                res["date"] = utils.date2str(info["mod_date"])
             self.res_and_fini(res)
             raise gen.Return(False)
         raise gen.Return(True)
--- trunk/src/appserver/apps/user_srv_d/handlers/helper_handler.py
+++ trunk/src/appserver/apps/user_srv_d/handlers/helper_handler.py
@@ -80,8 +80,9 @@ class HelperHandler(xmq_web_handler.XMQWebHandler):
                             self.dump_req(), str(info))
             res["status"] = ec
             if ec == error_codes.EC_LOGIN_IN_OTHER_PHONE:
-                res["x_os_name"] = info["device_model"]
+                res["device_model"] = info["device_model"]
                 res["msg"] = "您的账号已经在另一台手机登陆"
+                res["date"] = utils.date2str(info["mod_date"])
             self.res_and_fini(res)
             raise gen.Return(False)
         raise gen.Return(True)

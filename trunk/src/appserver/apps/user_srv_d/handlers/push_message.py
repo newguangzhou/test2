28443135839fda3c176c19edf030fd910958e7f5
龙 <bingodongtian@gmail.com>
2017-06-29 10:40:17 +0800

    push测试

--- trunk/src/appserver/apps/user_srv_d/handlers/push_message.py
+++ trunk/src/appserver/apps/user_srv_d/handlers/push_message.py
@@ -46,14 +46,14 @@ class PushMessageCmd(HelperHandler):
         msg = push_msg.new_pet_not_home_msg()
 
         try:
-            yield msg_rpc.push_android(uids=str(uid),
-                                   payload=msg,
-                                       pass_through=1
-                                       )
-
-            yield msg_rpc.push_ios(uids=str(uid),
-                                   payload=msg
-                                   )
+            # yield msg_rpc.push_android(uids=str(uid),
+            #                        payload=msg,
+            #                            pass_through=1
+            #                            )
+            #
+            # yield msg_rpc.push_ios(uids=str(uid),
+            #                        payload=msg
+            #                        )
             yield msg_rpc.push_ios_useraccount(uids=str(uid),
                                             payload=msg)
         except Exception, e:

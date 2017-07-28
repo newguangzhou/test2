5ff48fbe3adfd42ddb0dd87dbaf7f7eb15606119
yawei <xdjm562@qq.com>
2017-07-28 21:23:52 +0800

    fix battery bug

--- trunk/src/appserver/apps/terminal_srv_d/terminal_handler.py
+++ trunk/src/appserver/apps/terminal_srv_d/terminal_handler.py
@@ -313,7 +313,7 @@ class TerminalHandler:
         device_info= yield self.new_device_dao.get_device_info(pk.imei,("battery_status",))
         if device_info is not None:
             if not utils.battery_status_isequal(device_info.get("battery_status",0),battery_status) :
-                yield self.update_device_info(pk.imei,{"battery_status":battery_status})
+                yield self.new_device_dao.update_device_info(pk.imei,{"battery_status":battery_status})
                 yield self._SendBatteryMsg(pk.imei, pk.electric_quantity,
                                            battery_status, now_time)
         if pet_info is not None:
@@ -506,7 +506,7 @@ class TerminalHandler:
         device_info= yield  self.new_device_dao.get_device_info(pk.imei,("battery_status",))
         if device_info is not None:
             if not utils.battery_status_isequal(device_info.get("battery_status", 0),battery_status) :
-                yield self.update_device_info(pk.imei,{"battery_status":battery_status})
+                yield self.new_device_dao.update_device_info(pk.imei,{"battery_status":battery_status})
                 yield self._SendBatteryMsg(pk.imei, pk.electric_quantity,
                                            battery_status, now_time)
 

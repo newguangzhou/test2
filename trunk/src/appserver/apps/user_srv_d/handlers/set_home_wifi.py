25f5306cbe2293b348e9c4c357b29b2cfb6ec0e6
龙 <bingodongtian@gmail.com>
2017-08-17 17:20:36 +0800

    set_home_wifi

--- trunk/src/appserver/apps/user_srv_d/handlers/set_home_wifi.py
+++ trunk/src/appserver/apps/user_srv_d/handlers/set_home_wifi.py
@@ -74,8 +74,70 @@ class SetHomeWifi(HelperHandler):
         logging.debug("SetHomeWifi, success %s", self.dump_req())
         self.res_and_fini(res)
 
+
+
+    @gen.coroutine
+    def _deal_request_post(self):
+        logging.debug("SetHomeWifi, %s", self.dump_req())
+
+        self.set_header("Content-Type", "application/json; charset=utf-8")
+        user_dao = self.settings["user_dao"]
+        pet_dao = self.settings["pet_dao"]
+        terminal_rpc = self.settings["terminal_rpc"]
+        conf = self.settings["appconfig"]
+        res = {"status": error_codes.EC_SUCCESS}
+
+        uid = None
+        wifi_ssid = None
+        wifi_bssid = None
+        arround_wifilist=None
+        # wifi_power = None
+        try:
+            uid = int(self.get_argument("uid"))
+            token = self.get_argument("token")
+            st = yield self.check_token("SetHomeWifi", res, uid, token)
+            if not st:
+               return
+            wifi_ssid = self.get_argument("wifi_ssid")
+            wifi_bssid = self.get_argument("wifi_bssid")
+            logging.debug("arround_wifi",self.request.body)
+            # wifi_power = self.get_argument("wifi_power")
+        except Exception, e:
+            logging.warning("SetHomeWifi, invalid args, %s %s",
+                            self.dump_req(), str(e))
+            res["status"] = error_codes.EC_INVALID_ARGS
+            self.res_and_fini(res)
+            return
+
+        try:
+
+            #info = yield pet_dao.get_user_pets(uid, ("pet_id", ))
+            #if info is None:
+            #    logging.warning("SetHomeWifi, not found, %s", self.dump_req())
+            #    res["status"] = error_codes.EC_PET_NOT_EXIST
+            #    self.res_and_fini(res)
+            #    return
+
+            set_res = yield pet_dao.set_home_wifi(uid, {"wifi_ssid": wifi_ssid,
+                                                        "wifi_bssid":
+                                                        wifi_bssid})
+            if set_res.matched_count <= 0:
+                logging.warning("SetHomeWifi, set fail, %s", self.dump_req())
+                res["status"] = error_codes.EC_SYS_ERROR
+                self.res_and_fini(res)
+        except Exception, e:
+            logging.warning("SetHomeWifi, error, %s %s", self.dump_req(),
+                            self.dump_exp(e))
+            res["status"] = error_codes.EC_SYS_ERROR
+            self.res_and_fini(res)
+            return
+
+# 成功
+        logging.debug("SetHomeWifi, success %s", self.dump_req())
+        self.res_and_fini(res)
+
     def post(self):
-        return self._deal_request()
+        return self._deal_request_post()
 
     def get(self):
         return self._deal_request()

aed48030e9ca95776b03c0164cc61b9e442b4747
server2 <server2@xmq.com>
2017-09-12 14:42:18 +0800

    change config

--- trunk/tools/mongdb2/init_super_user.sh
+++ trunk/tools/mongdb2/init_super_user.sh
@@ -13,7 +13,7 @@ mongodir=$4
 
 shift 4
 
-eval_str="db.getMongo().getDB('admin').createUser({user:'${username}', pwd:'${passwd}', roles:[{role:'readWriteAnyDatabase', db:'admin'}, {role:'userAdminAnyDatabase', db:'admin'}, {role:'clusterAdmin', db:'admin'}, {role:'dbAdminAnyDatabase', db:'admin'}]})"
+eval_str="db.getMongo().getDB('admin').createUser({user:'${username}', pwd:'mgdb8w34asdadat51!((', roles:[{role:'readWriteAnyDatabase', db:'admin'}, {role:'userAdminAnyDatabase', db:'admin'}, {role:'clusterAdmin', db:'admin'}, {role:'dbAdminAnyDatabase', db:'admin'}]})"
 
 $mongodir/mongo --host $LAN_IP --port $port $* --eval "$eval_str"
 
